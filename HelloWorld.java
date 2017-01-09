22f80b5e4565b0ba7db2f4835298b3e056707e52
EC2 Default User <ec2-user@ip-172-31-29-227.us-west-2.compute.internal>
2017-01-09 18:57:56 +0000

    vinay

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World New 11111111111111111");
+      System.out.println("Hello, World New year 2017");
    }
 }
